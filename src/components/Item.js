1d11baaa7c0e908340f91e066720267ced9fd742
Timothy Ryon <tim@timryon.com>
2024-01-23 15:03:41 -0500

    working dark mode

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+const [inCart, setInCart] = useState(false);
+
+  function handleClick() {
+    setInCart((inCart) => !inCart);
+  }
+
+  const itemClassName = inCart ? "in-cart" : "";
+
   return (
     <li className="">
       <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <span className= { itemClassName }>{category}</span>
+      <button className="add" onClick = { handleClick }> { inCart ? "Add to Cart" : "Remove from Cart" } </button>
     </li>
   );
 }
